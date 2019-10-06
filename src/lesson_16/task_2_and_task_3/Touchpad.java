1471f0963d5cd6cc694dfc0b660cd04d258c034d
Daniil <danikkim19@gmail.com>
2019-10-06 17:25:08 +0300

    g

--- src/lesson_16/task_2_and_task_3/Touchpad.java
+++ src/lesson_16/task_2_and_task_3/Touchpad.java
@@ -4,14 +4,17 @@ import java.io.Serializable;
 
 public class Touchpad implements Serializable {
 
+    private static final long serialVersionUID = 12466557645L;
+
     private double sensitivity;
+    private double area;
 
     public Touchpad(double sensitivity, double area) {
         this.sensitivity = sensitivity;
         this.area = area;
     }
 
-    private double area;
+
 
     @Override
     public String toString() {
